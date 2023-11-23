c41c424310906908db877010c9db938280501a22
bryan <bryanhernandez3233@gmail.com>
2023-11-22 20:52:26 -0600

    added the settings button to the home page. As well as added the settings backend file and the frontend file. Also added to map the scroll view and text views for dislaying event data.

--- app/src/main/java/com/example/raiderreminder/HomeFragment.java
+++ app/src/main/java/com/example/raiderreminder/HomeFragment.java
@@ -3,10 +3,14 @@ package com.example.raiderreminder;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class HomeFragment extends Fragment {
@@ -16,7 +20,27 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        FloatingActionButton floatingSettingButton = view.findViewById(R.id.floatingSettingButton);
+        floatingSettingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Replace the current fragment with SettingsFragment
+                replaceFragment(new SettingsFragment());
+            }
+        });
+
+        return view;
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        if (getActivity() != null) {
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.frame_layout, fragment);
+            fragmentTransaction.addToBackStack(null);  // Optional: Add to back stack
+            fragmentTransaction.commit();
+        }
     }
 }
