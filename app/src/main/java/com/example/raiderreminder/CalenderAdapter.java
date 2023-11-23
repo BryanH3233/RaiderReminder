95ec8435e24a911fba89dadc35e76f65bfbc1952
Uriel6u <uriel.esquivel.6u@gmail.com>
2023-11-22 21:26:26 -0600

    Added calender xml and backend. Next and previous buttons now work. Comments for Calender java and Xml files added. Image for left/Right arrows added

--- app/src/main/java/com/example/raiderreminder/CalenderAdapter.java
+++ app/src/main/java/com/example/raiderreminder/CalenderAdapter.java
@@ -11,41 +11,46 @@ import java.util.ArrayList;
 
 class CalenderAdapter extends RecyclerView.Adapter<CalenderViewHolder>
 {
+    // List to store the days of the month
     private final ArrayList<String> daysOfMonth;
+    // Listener to handle item click events
     private final OnItemListener onItemListener;
-
+    // Constructor to initialize the adapter with data and click listener
    public CalenderAdapter(ArrayList<String> daysOfMonth, OnItemListener onItemListener)
    {
         this.daysOfMonth = daysOfMonth;
+       // Set a click listener on the item view
         this.onItemListener = onItemListener;
     }
-
+    // Create new views (invoked by the layout manager)
     @NonNull
     @Override
     public CalenderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
     {
+        // Inflate the item layout and set its height dynamically
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.calender_cell, parent,  false);
         ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
         layoutParams.height = (int) (parent.getHeight() * 0.166666666);
-
+        // Create a new ViewHolder with the inflated view and click listener
         return new CalenderViewHolder(view, onItemListener);
     }
-
+    // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(@NonNull CalenderViewHolder holder, int position)
     {
         holder.dayOfMonth.setText(daysOfMonth.get(position));
     }
-
+    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount()
     {
         return daysOfMonth.size();
     }
-
+    // Interface to handle item click events
     public interface  OnItemListener
     {
+
         void onItemClick(int position, String dayText);
 
     }
