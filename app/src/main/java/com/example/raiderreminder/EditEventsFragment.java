8d003e46d7a9bf6bde137aba4bff094971702f80
Bryan <bryanhernandez3233@gmail.com>
2023-11-28 19:21:20 -0600

    added tool bar for edit events also delete button for edit events edited the color for delete icon got the toolbar to display in the back end

--- app/src/main/java/com/example/raiderreminder/EditEventsFragment.java
+++ app/src/main/java/com/example/raiderreminder/EditEventsFragment.java
@@ -2,9 +2,13 @@ package com.example.raiderreminder;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -16,7 +20,33 @@ public class EditEventsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_editevents, container, false);
+
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+        View view = inflater.inflate(R.layout.fragment_editevents, container, false);
+
+        //Create the home tool bar
+        Toolbar editeventsToolBar = view.findViewById(R.id.editeventstoolbar);
+
+        // Ensure that the parent activity is an AppCompatActivity
+        if (getActivity() instanceof AppCompatActivity) {
+            AppCompatActivity appCompatActivity = (AppCompatActivity) getActivity();
+            appCompatActivity.setSupportActionBar(editeventsToolBar);
+
+            // Set the title of the toolbar
+            appCompatActivity.getSupportActionBar().setTitle("Edit Events");
+        }
+
+
+        return view;
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu resource
+        inflater.inflate(R.menu.editeventstoolbar_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }
